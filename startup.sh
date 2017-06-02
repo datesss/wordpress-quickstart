1f3365651160ed490f566c9a8388f8e1d42b3ff7
dates <jake.brooks@midsummerish.com>
2017-06-02 12:21:16 -0400

    adding to my bash prof

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 docker-compose up -d
 alias wp="docker-compose run cli wp"
 export $(cat .env | xargs)
+echo "alias wp='docker-compose run cli wp';" >> ~/.bash_profile
 wp core install --url="localhost" --title=$WP_SITE_TITLE --admin_user=$WP_USERNAME --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
