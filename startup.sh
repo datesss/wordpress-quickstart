b5a5088f3b9cf98d00380f1957bda27fcae13ed7
dates <jake.brooks@midsummerish.com>
2017-08-10 18:26:10 -0400

    editing to work with reverse proxy

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 docker-compose up -d
-alias wp="docker-compose run cli wp"
+alias wpd="docker-compose run cli wp"
 export $(cat .env | xargs)
-echo "alias wp='docker-compose run cli wp';" >> ~/.bash_profile
-wp core install --url="localhost" --title=$WP_SITE_TITLE --admin_user=$WP_USERNAME --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
+echo "alias wpd='docker-compose run cli wp';" >> ~/.bash_profile
+wp core install --url=$WP_SITE_URL --title=$WP_SITE_TITLE --admin_user=$WP_USERNAME --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
