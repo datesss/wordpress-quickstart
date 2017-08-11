b2c66a48b1c292bf7def7ecf1c2e2d05dfaac134
dates <jake.brooks@midsummerish.com>
2017-08-11 10:59:15 -0400

    need to connect to right db

--- startup.sh
+++ startup.sh
@@ -1,5 +1,12 @@
-docker-compose up -d
+export $(cat .env.local | xargs)
+# set -a # export all variables created next
+# source .env.local
+# set +a # stop exporting
+
+docker-machine env -u
+eval $(docker-machine env -u)
+
 alias wpd="docker-compose run cli wp"
-export $(cat .env | xargs)
+docker-compose -f local.yml up -d
 echo "alias wpd='docker-compose run cli wp';" >> ~/.bash_profile
 wp core install --url=$WP_SITE_URL --title=$WP_SITE_TITLE --admin_user=$WP_USERNAME --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL
